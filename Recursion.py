7e095ac2fcec58cdf93964df68a7027072e90ba4
joseph003 <45079915+joseph003@users.noreply.github.com>
2018-11-16 11:13:34 -0600

    Create Recursion.py

---
+++ Recursion.py
@@ -0,0 +1,11 @@
+from math import factorial 
+
+def factorial:
+  num = int(raw_input('Please enter a number: '))
+if num <= 0:
+   print("Factorial does not exist")
+elif num > 15:
+   print("I can't count that high")
+else:
+   return factorial(num)
+   print str(num)
